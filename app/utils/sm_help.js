c97717499d0cad678ad4bb7dda73d0a218f184ad
JordanVScher <jordanvscher@hotmail.com>
2019-06-24 13:48:03 -0300

    Remove log

--- app/utils/sm_help.js
+++ app/utils/sm_help.js
@@ -340,10 +340,6 @@ console.log(surveysInfo);
 // what to do with the form that was just answered
 async function newSurveyResponse(event) {
 	const responses = await smAPI.getResponseWithAnswers(event.filter_id, event.object_id); console.log('responses', JSON.stringify(responses, null, 2)); // get details of the event
-	console.log(surveysInfo.module3.id);
-	console.log(responses.survey_id);
-
-
 	switch (responses.survey_id) { // which survey was answered?
 	case surveysInfo.sondagemPre.id:
 		await handleSondagem(responses, 'pre', surveysMaps.sondagemPre);
@@ -364,9 +360,6 @@ async function newSurveyResponse(event) {
 		await handleAtividade(responses, 'atividade_modulo2');
 		break;
 	case surveysInfo.module3.id:
-
-		console.log('lol entre aqui');
-
 		await handleAtividade(responses, 'atividade_modulo3');
 		break;
 	case surveysInfo.indicacao360.id:
