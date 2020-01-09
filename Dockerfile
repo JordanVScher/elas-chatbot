c6e921e1bb6f3cd9c369560f820fa7263ae8c72a
JordanVScher <jordanvscher@hotmail.com>
2020-01-09 16:36:18 -0300

    Docker

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:11.13.0
 ENV NPM_CONFIG_LOGLEVEL warn
 
-EXPOSE 2300
+EXPOSE 2700
 
 USER root
 RUN mkdir src
@@ -10,13 +10,14 @@ RUN apt-get update
 RUN apt-get install -y runit
 RUN apt-get install -y build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
 
+
 USER node
 ADD package.json /src/
 WORKDIR /src
 
-RUN npm install bottender dotenv
-RUN npm install
-ADD . /src
+#RUN npm install bottender dotenv
+#RUN npm install
+#ADD . /src
 
 USER root
 COPY services/ /etc/service/
--- sample-run-container.sh
+++ sample-run-container.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 # arquivo de exemplo para iniciar o container
-export SOURCE_DIR='/home/appcivico/projects/elas-chatbot'
-export DATA_DIR='/tmp/elas-chatbot/data/'
+export SOURCE_DIR='/home/jordan/elas-chatbot-homol'
+export DATA_DIR='/tmp/elas-chatbot-homol/data/'
 
 # confira o seu ip usando ifconfig docker0|grep 'inet addr:'
 export DOCKER_LAN_IP=$(ifconfig docker0 | grep 'inet addr:' | awk '{ split($2,a,":"); print a[2] }')
 
-# porta que ser   feito o bind
-export LISTEN_PORT=1006
-sequelize db:migrate
+# porta que sera feito o bind
+export LISTEN_PORT=2002
 
-docker run --name elas-chatbot \
+docker run --name prep-chatbot \
  -v $SOURCE_DIR:/src -v $DATA_DR:/data \
- -p $DOCKER_LAN_IP:$LISTEN_PORT:4000 \
+ -p $DOCKER_LAN_IP:$LISTEN_PORT:2700 \
  --cpu-shares=512 \
  --memory 1800m -dit --restart unless-stopped appcivico/elas-chatbot
