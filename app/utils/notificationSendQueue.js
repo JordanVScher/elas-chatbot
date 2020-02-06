eae0349a40c9dff6ac1991dcaf6f0f1e0e84ffa6
JordanVScher <jordanvscher@hotmail.com>
2020-02-06 15:38:02 -0300

    Bug

--- app/utils/notificationSendQueue.js
+++ app/utils/notificationSendQueue.js
@@ -361,7 +361,8 @@ async function checkShouldSendNotification(notification, moduleDates, today, not
 	}
 
 	if (!currentRule) {
-		return sentryError('currentRule undefined!', JSON.stringify({ currentNotification: notification }, null, 2));
+		return false;
+		// return sentryError('currentRule undefined!', JSON.stringify({ currentNotification: notification }, null, 2));
 	}
 	const dateToSend = await rules.getSendDate(ourTurma, currentRule); // the date to send
 	const moduloDate = new Date(ourTurma[`modulo${currentRule.modulo}`]);
