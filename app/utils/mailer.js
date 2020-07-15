e20233f36dc98dd39425d9a2df7377c2e359a01e
Jordan Victor Scher <jordanvscher@hotmail.com>
2020-07-15 14:23:03 -0300

    retry

--- app/utils/mailer.js
+++ app/utils/mailer.js
@@ -101,3 +101,44 @@ async function sendReport(res, text, subject, filename) {
 module.exports = {
 	sendHTMLMail, sendHTMLFile, sendReport,
 };
+
+
+----------------------------------------------
+id                 | 6259
+notification_type  | 1
+aluno_id           | 16
+indicado_id        |
+sent_at            | 2020-07-15 17:01:54.779+00
+error              | {"chatbot":{"sent":false,"msg":"Aluna não está vinculada no chatbot","moment":"2020-07-15T17:01:54.786Z"}}
+created_at         | 2020-07-10 14:22:28.588+00
+updated_at         | 2020-07-15 17:01:54.786+00
+check_answered     |
+turma_id           | 22
+sent_at_chatbot    |
+additional_details |
+-[ RECORD 2 ]------+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+id                 | 6237
+notification_type  | 1
+aluno_id           | 215
+indicado_id        |
+sent_at            | 2020-07-15 17:01:54.04+00
+error              |
+created_at         | 2020-07-10 13:25:22.235+00
+updated_at         | 2020-07-15 17:01:54.269+00
+check_answered     |
+turma_id           | 22
+sent_at_chatbot    | 2020-07-15 17:01:54.268+00
+additional_details |
+-[ RECORD 3 ]------+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+id                 | 8601
+notification_type  | 1
+aluno_id           | 20
+indicado_id        |
+sent_at            | 2020-07-15 17:01:55.161+00
+error              | {"chatbot":{"sent":false,"error":"Error: Messenger API - 10 OAuthException (#10) This message is sent outside of allowed window. Learn more about the new policy here: https://developers.facebook.com/docs/messenger-platform/policy-overview\n    at handleError (/src/node_modules/messaging-api-messenger/lib/MessengerClient.js:91:11)\n    at processTicksAndRejections (internal/process/task_queues.js:86:5)","moment":"2020-07-15T17:01:55.260Z"}}
+created_at         | 2020-07-15 16:58:33.716+00
+updated_at         | 2020-07-15 17:01:55.26+00
+check_answered     |
+turma_id           | 22
+sent_at_chatbot    |
+additional_details |
