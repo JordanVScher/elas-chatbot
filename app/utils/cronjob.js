2bbd9fc1fe736af4b5bf1b48c9e5a41e8c92192d
Jordan Victor Scher <jordanvscher@hotmail.com>
2020-07-15 14:24:01 -0300

    asd

--- app/utils/cronjob.js
+++ app/utils/cronjob.js
@@ -67,7 +67,7 @@ const sendNotificationCron = new CronJob(
 			await sentryError('Error on sendNotificationCron', error);
 		}
 	}, (() => { console.log('Crontab sendNotificationCron stopped.'); }),
-	true, 'America/Sao_Paulo', false, false,
+	true, 'America/Sao_Paulo', false, true,
 );
 
 const updateTurmasCron = new CronJob(
