f6ed9aa84d7e0b5a36ad439891651a9fe0815019
Lucas Ansei <lucas.ansei@appcivico.com>
2020-10-13 18:14:06 -0300

    revert cron window

--- app/utils/cronjob.js
+++ app/utils/cronjob.js
@@ -53,8 +53,7 @@ const sendWarningAlunasCron = new CronJob(
 );
 
 const sendNotificationCron = new CronJob(
-	// '00 00 7-22/1 * * *', async () => {
-	'*/10 * * * *', async () => {
+	'00 00 7-22/1 * * *', async () => {
 		await cronLogs.create({ runAt: new Date(), name: 'sendNotificationCron' }).then((r) => r).catch((err) => sentryError('Erro no update do cronLogs', err));
 		console.log(`Running sendNotificationCron - ${new Date()}`);
 		try {
