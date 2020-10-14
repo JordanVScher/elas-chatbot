570df4104dcfe7c494458f66b6e6f1661fee267b
Lucas Ansei <lucas.ansei@appcivico.com>
2020-10-14 17:12:50 -0300

    Updating email query

--- app/utils/notificationSendQueue.js
+++ app/utils/notificationSendQueue.js
@@ -263,7 +263,7 @@ async function getQueue(turmaID, alunoID, notificationType) {
 		sent_at_chatbot: null,
 		error: null,
         turma_id: 26,
-        notification_type: 1
+        notification_type: { [Op.or]: [1, 2] }
 	};
 
 	if (notificationType) query.notification_type = notificationType;
